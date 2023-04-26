65b341736c1326f045db79724563aff8e45a3423
Geoffrey MORIZOT <geoffreymorizot@MBP-de-Geoffrey.lan>
2023-04-26 16:02:19 +0200

    refacto: useIntersectionObserver hook factorization

--- composables/useIntersectionObserver.ts
+++ composables/useIntersectionObserver.ts
@@ -1,45 +1,70 @@
+interface ElementToObserve {
+  el: HTMLElement
+  callback: (() => void) | (() => gsap.core.Timeline)
+}
+
 export function useIntersectionObserver() {
   const options = ref<IntersectionObserverInit>({ threshold: 0.5 })
 
   function observe(
     elements:
-      | Ref<NodeListOf<Element> | undefined>
-      | Ref<Array<HTMLElement> | undefined>
+      | Ref<NodeListOf<Element> | HTMLElement[]>
       | Ref<HTMLElement | undefined>,
-    cb: () => gsap.core.Timeline | (() => void)
+    callback: (() => void) | (() => gsap.core.Timeline)
   ) {
-    if (!elements.value)
-      throw new Error('Element in intersection observer is undefined')
-    if (elements.value instanceof HTMLElement) {
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('isIntersecting')
-            cb()
-            observer.unobserve(entry.target)
-            entry.target.classList.remove('isIntersecting')
-          }
-        })
-      }, options.value)
-      // Start observing the element
-      observer.observe(elements.value)
+    if (!elements.value) {
+      throw new Error(
+        'useIntersectionObserver: the element(s) to observe is undefined'
+      )
+    }
+
+    const elementsToObserve: ElementToObserve[] = []
+
+    if (elements.value instanceof NodeList) {
+      for (const element of elements.value) {
+        if (element instanceof HTMLElement) {
+          elementsToObserve.push({
+            el: element,
+            callback,
+          })
+        }
+      }
+    } else if (Array.isArray(elements.value)) {
+      for (const element of elements.value) {
+        if (element instanceof HTMLElement) {
+          elementsToObserve.push({
+            el: element,
+            callback,
+          })
+        }
+      }
     } else {
-      elements.value.forEach((element) => {
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.classList.add('isIntersecting')
-              cb()
+      elementsToObserve.push({
+        el: elements.value,
+        callback,
+      })
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('isIntersecting')
+          elementsToObserve.forEach((elementToObserve) => {
+            if (elementToObserve.el === entry.target) {
+              elementToObserve.callback()
               observer.unobserve(entry.target)
               entry.target.classList.remove('isIntersecting')
             }
           })
-        }, options.value)
-        // Start observing the element
-        observer.observe(element)
+        }
       })
-    }
+    }, options.value)
+
+    elementsToObserve.forEach((elementToObserve) => {
+      observer.observe(elementToObserve.el)
+    })
   }
+
   return {
     observe,
     options,
